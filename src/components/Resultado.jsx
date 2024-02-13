9165d22c78697e461eadea5cbcd209ca86bd5b0e
boitaro <boitaro@hotmail.com>
2024-02-13 00:07:38 -0600

    first commit

---
+++ src/components/Resultado.jsx
@@ -0,0 +1,51 @@
+//import React from 'react'
+import styled from "@emotion/styled"
+
+const Contenedor = styled.div`
+    color: #FFF;
+    font-family: 'Lato',sans-serif;
+    display: flex;
+    align-items: center;
+    gap: 3rem;
+    margin-top: 30px;
+`
+const Imagen = styled.img`
+    display: block;
+    width: 120px;
+`
+
+const Texto = styled.p`
+    font-size: 15px;
+    span{
+        font-weight: 700;
+    }
+`
+
+const Precio = styled.p`
+    font-size: 24px;
+    span {
+        font-weight: 700;
+    }
+`
+
+const Resultado = ({resultado}) => {
+  const {PRICE, HIGHDAY, LOWDAY,CHANGEPCT24HOUR, IMAGEURL, LASTUPDATE} =  resultado   
+  console.log(resultado)
+    return (
+    <Contenedor>
+      <Imagen 
+        src={`https://cryptocompare.com/${IMAGEURL}`} 
+        alt="Imagen Cripto"       
+      />
+      <div>
+        <Precio>El precio es de:<br /> <span>{PRICE}</span> </Precio>
+        <Texto>Precio más alto del día: <span>{HIGHDAY}</span> </Texto>
+        <Texto>El precio más bajo del día: <span>{LOWDAY}</span> </Texto>
+        <Texto>Variación últimas 24 horas: <span>{CHANGEPCT24HOUR}</span> </Texto>
+        <Texto>Última actualización: <span>{LASTUPDATE}</span> </Texto>
+      </div>
+    </Contenedor>
+  )
+}
+
+export default Resultado
